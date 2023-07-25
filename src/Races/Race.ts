6c9907fa29c2d5e2f851b3cfc206d7aae10b6612
Felipe <felipe.raindo@gmail.com>
2023-07-25 03:46:30 -0300

    requisito 1

---
+++ src/Races/Race.ts
@@ -0,0 +1,23 @@
+export default abstract class Race {
+  private readonly _name: string;
+  private readonly _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  public abstract get maxLifePoints(): number;
+}
