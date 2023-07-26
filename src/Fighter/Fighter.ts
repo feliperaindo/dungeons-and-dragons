fc725dc3c6c196f2330a5786483b54f8b8051810
Felipe <felipe.raindo@gmail.com>
2023-07-26 01:45:04 -0300

    req 06 done

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,16 @@
+import { EnergyType } from '../Energy';
+
+export default interface Fighter {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: EnergyType;
+
+  levelUp() : void;
+
+  attack(enemy: Fighter) : void;
+
+  special?(enemy: Fighter) : void;
+
+  receiveDamage(attackPoints: number) : number;
+}
